ba20c20569878f5fc0dc729af4c6dc694350c3db
VortX307 <vortx307@gmail.com>
2026-08-23 12:14:51 +0530

    Lab assignment 1, Question 1

---
+++ lab_assignment_1/hello_world.c
@@ -0,0 +1,10 @@
+#include <stdio.h> //Standard Input output library for basic functions
+
+
+int main() //Initializing entry point
+{
+    printf("Hello World");
+
+    
+    return 0; //Safe exit code 0
+}
