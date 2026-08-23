5c126f9d3d0ed0dd280111b500cadd27d31918a2
VortX307 <vortx307@gmail.com>
2026-08-23 13:29:26 +0530

    Added UES103 C lab assignment 1 question 2

---
+++ UES103/c/lab_assignment_1/integer_input.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Enter an integer: \n");
+    int num = 0; 
+    scanf("%d", &num);
+    printf("The number you entered was: %d", num);
+    return 0;
+}
